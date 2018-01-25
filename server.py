6c343f077d4392d49869acd33ff6f1115182b747
Gupta <606386@Cognizant.com>
2018-01-25 15:33:11 +0530

    get users API.

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from models.user import User
 
 from sqlalchemy import or_
 from database.database import scoped, engine
+from controllers.users import UsersController
 
 app = Flask(__name__)
 
@@ -12,15 +13,21 @@ def index():
 
 @app.route("/users", methods = ['GET'])
 def users():
-    users = [u.as_dict() for u in User.query.all()]
-    return jsonify(users)
+    usersController = UsersController()
+    return jsonify(usersController.getUsers())
 
-@app.route("/users/searchByName", methods = ['POST'])
+@app.route("/search", methods = ['POST'])
 def search():
-    name=request.json["name"]
-    users=[u.as_dict() for u in User.query.filter(or_(User.first_name.ilike('%'+name+'%'), User.last_name.ilike('%'+name+'%')))]
-    return jsonify(users)
+    name = request.json["name"]
+    usersController = UsersController()
+    return jsonify(usersController.searchByName(name))
 
+@app.route("/user", methods = ['GET'])
+def getUserById():
+   id = request.args.get('id')
+   usersController = UsersController()
+   return jsonify(usersController.getUserById(id))
+   
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     scoped.remove()
